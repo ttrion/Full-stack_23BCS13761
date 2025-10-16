ad68e4cc7de0cd70a8316ce15b8cb294b1150dc2
ttrion <23bcs13761@cuchd.in>
2025-10-16 10:43:59 +0530

    Add files via upload

---
+++ Experiment 7/src/main/java/com/portal/academic/student_academic_portal/StudentAcademicPortalApplication.java
@@ -0,0 +1,13 @@
+package com.portal.academic.student_academic_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAcademicPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAcademicPortalApplication.class, args);
+	}
+
+}
