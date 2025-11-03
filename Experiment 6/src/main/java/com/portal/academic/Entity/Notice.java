c36e30e5feeaffff16772069ff73dc0d7f559906
ttrion <23bcs13761@cuchd.in>
2025-11-03 12:40:13 +0530

    Add files via upload

---
+++ Experiment 6/src/main/java/com/portal/academic/Entity/Notice.java
@@ -0,0 +1,20 @@
+package com.portal.academic.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import java.time.LocalDateTime;
+@Entity
+@Data
+public class Notice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+    private String category;
+    private boolean isUrgent;
+    private LocalDateTime publicationDate;
+}
